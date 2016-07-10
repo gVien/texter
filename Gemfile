4effa4406c67d672b4390dc341614fab96123df3
gVien <wavetech86@gmail.com>
2016-07-09 22:39:42 -0700

    add shared folder, error msg, header, bootstrap & refactor form

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+
+gem 'bootstrap-sass', '~> 3.3.5'  #converts Less to Sass, since Rails asset pipeline supports the Sass language by default
+
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
--- app/models/text.rb
+++ app/models/text.rb
@@ -5,6 +5,6 @@ class Text
   attribute :message
 
   attr_accessor :phone_number, :message
-  validates :phone_number, presence: true, format: { with: /\d{10}/, message: "Phone number must be 10 continuous digits including the area code, e.g. 1234567890" }
+  validates :phone_number, presence: true, format: { with: /\d{10}/, message: "must be 10 continuous digits including the area code, e.g. 1234567890" }
   validates :message, presence: true
 end
